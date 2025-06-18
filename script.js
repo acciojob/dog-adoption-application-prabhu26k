256e8f34a21a5add5ca6fe293a92dc9f11b62ae0
Prabhukumar <163394327+prabhu26k@users.noreply.github.com>
2025-06-18 12:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//your code here
-document.getElementById('adoptionForm').addEventListener('submit', function (event) {
+document.getElementById('adoptionForm').addEventListener('submit', function(event) {
   event.preventDefault();
   alert("Form submitted successfully!");
-  // You can add logic here to send data to a backend or show the submitted values.
 });
