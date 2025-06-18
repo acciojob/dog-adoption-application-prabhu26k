2d6d0b57a84cc0579b34412fc0aee2b5c893585f
Prabhukumar <163394327+prabhu26k@users.noreply.github.com>
2025-06-18 13:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.getElementById('breeds').addEventListener('change', function () {
+  const selected = Array.from(this.selectedOptions);
+  if (selected.length > 2) {
+    selected[selected.length - 1].selected = false;
+    alert("You can only select up to 2 breeds.");
+  }
+});
+
 document.getElementById('dogAdoptionForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -5,7 +13,7 @@ document.getElementById('dogAdoptionForm').addEventListener('submit', function(e
   const phone = document.getElementById('phone').value;
   const address = document.getElementById('address').value;
   const livedWithDog = document.getElementById('livedWithDog').checked;
-  
+
   const breedOptions = document.getElementById('breeds').selectedOptions;
   const selectedBreeds = Array.from(breedOptions).map(option => option.value);
 
